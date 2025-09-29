640095bca7a1bb3291bff6fe836711db00687a09
Fuelwild796 <165859108+Fuelwild796@users.noreply.github.com>
2025-09-29 10:25:23 -0300

    Initial commit

---
+++ src/main/java/com/educandoweb/sistemagestaoestoque/SistemaGestaoEstoqueApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.sistemagestaoestoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGestaoEstoqueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaGestaoEstoqueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/educandoweb/sistemagestaoestoque/SistemaGestaoEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.sistemagestaoestoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGestaoEstoqueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
