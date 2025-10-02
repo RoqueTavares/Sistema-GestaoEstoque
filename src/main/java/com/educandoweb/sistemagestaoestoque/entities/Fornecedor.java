2a423670dbc654027dfd44475d529a3430ba5f05
Fuelwild796 <165859108+Fuelwild796@users.noreply.github.com>
2025-10-01 22:39:56 -0300

    created fornecedor entity

---
+++ src/main/java/com/educandoweb/sistemagestaoestoque/entities/Fornecedor.java
@@ -0,0 +1,22 @@
+package com.educandoweb.sistemagestaoestoque.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name ="tb_Fornecedores" )
+public class Fornecedor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String cnpj;
+    private String telefone;
+    private String email;
+}
